c25cf1b723b52708b8133b869d5ed09200e8024f
dvt <dvt451@gmail.com>
2026-02-04 13:40:43 +0400

    update vite-api

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
 	const [userToken, setUserToken] = useState(null);
 	const [user, setUser] = useState({});
 	const navigate = useNavigate();
-	const BASE_URL = 'http://localhost:5000';
+	const BASE_URL = import.meta.env.VITE_API_URL;
 
 	// Аналог getToken для web
 	const getToken = () => localStorage.getItem('token');
