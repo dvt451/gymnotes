0fd62840806fabd8077c1f529e62228c34aed394
dvt <dvt451@gmail.com>
2026-02-10 19:30:44 +0400

    Update

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -44,8 +44,7 @@ const userSchema = new mongoose.Schema({
 	weight: { type: Number },
 	email: { type: String, required: true, unique: true, lowercase: true },
 	password: { type: String },
-	nutritions:
-		trainingfiles: [trainingFileSchema],
+	trainingfiles: [trainingFileSchema],
 	trainingOrder: {
 		type: [String],
 		default: []
